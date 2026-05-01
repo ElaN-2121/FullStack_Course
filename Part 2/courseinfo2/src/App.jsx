fd1292f0a8e40c464bb7810b3a4cb699dfbe7865
elnathan <elnathannigussie@gmail.com>
2026-05-01 23:53:48 +0300

    added answer for exercises 2.4-5

--- Part 2/courseinfo2/src/App.jsx
+++ Part 2/courseinfo2/src/App.jsx
@@ -1,43 +1,58 @@
 import { useState } from 'react'
-
-const Course = ({course}) => {
-  const {name, parts} = course
-  let total = 0
-
-  return(
-    <>
-      <h1>{name}</h1>
-      {parts.map(part => <h3>{part.name} {part.exercises}</h3>)}
-      <h3>Total of <strong>{parts.reduce((sum, part) => sum + part.exercises, 0)} </strong>exercises</h3>
-    </>
-  )
-}
+import Course from './Course'
 
 const App = () => {
-  const course = {
-    id:1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
-    <Course course={course} />
+    <>
+    {courses.map(course => (
+      <Course key={course.id} course={course} />
+    ))}
+    </>
   )
 }
 
